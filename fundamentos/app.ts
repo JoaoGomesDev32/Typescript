c15b2c8383de5e0637eeb4e9361d35731f337ad6
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2026-02-21 12:35:35 +0000

    interfaces e objetos

--- fundamentos/app.ts
+++ fundamentos/app.ts
@@ -1,3 +1,5 @@
+/*
+variaveis e tipos
 let b: string = 'b';
 let n: number = 2;
 let x: boolean = true;
@@ -5,3 +7,36 @@ let x: boolean = true;
 let m: any = 2;
 m = "João";
 m = false;
+*/
+
+//interfaces e objetos
+interface Pessoa {
+	nome: string,
+	idade: number,
+	profissao?: string
+}
+
+const pessoa: Pessoa = {
+	nome: 'João',
+	idade: 36
+}
+
+const outraPessoa: Pessoa = {
+	nome: 'Paulo',
+	idade: 33,
+	profissao: 'Desenvolvedor'
+}
+
+const arrayPessoa: Array<Pessoa> = [
+	pessoa,
+	outraPessoa
+]
+
+const arrayNum: number[] = [
+	1, 2, 3
+]
+
+const arrayString: Array<string> = [
+	'1', '2', '3'
+]
+
