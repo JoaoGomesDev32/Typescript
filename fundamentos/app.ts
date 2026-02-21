c96c966006844889d84fa79544d19f733f325fa0
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2026-02-21 12:57:38 +0000

    estrutura de decisao

--- fundamentos/app.ts
+++ fundamentos/app.ts
@@ -1,3 +1,30 @@
+//decisoes e repeticoes
+const num: number = 15;
+
+if (num > 15) {
+	console.log('Num maior que 15')
+} else if (num === 15) {
+	console.log('Num igual a 15')
+} else {
+	console.log('Num menor que 15')
+}
+
+const typeUser = {
+	admin: 'Seja bem vindo admin',
+	student: 'Você é um estudante',
+	viewer: 'Você pode visualizar'
+}
+
+function validateUser(user: string) {
+	console.log(typeUser[user as keyof typeof typeUser])
+}
+
+const usuario = 'admin';
+
+validateUser(usuario)
+validateUser('student')
+validateUser('viewer')
+
 /*
 variaveis e tipos
 let b: string = 'b';
@@ -9,7 +36,7 @@ m = "João";
 m = false;
 */
 
-//interfaces e objetos
+/*interfaces e objetos
 interface Pessoa {
 	nome: string,
 	idade: number,
@@ -39,4 +66,4 @@ const arrayNum: number[] = [
 const arrayString: Array<string> = [
 	'1', '2', '3'
 ]
-
+*/
