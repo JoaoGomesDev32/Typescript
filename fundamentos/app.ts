6e86e6493d2e4f1bb847864de95fcee5e66ba7ad
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2026-02-22 13:51:09 +0000

    classes e tipos

--- fundamentos/app.ts
+++ fundamentos/app.ts
@@ -1,4 +1,34 @@
-class User {
+//Dio banking
+
+//name, accountNumber
+//depositar, sacar
+
+class Account {
+	name: string
+	accountNumber: number
+
+	constructor(name: string, accountNumber: number) {
+		this.name = name
+		this.accountNumber = accountNumber
+	}
+
+	deposit = () => {
+		console.log('Você depositou');
+	}
+
+	withdraw = () => {
+		console.log('Você sacou');
+	}
+}
+
+const newAccount: Account = new Account('João', 1);
+console.log(newAccount);
+
+const account: Account = new Account('Denize', 28)
+account.deposit();
+
+
+/*class User {
 	name: string = 'João'
 	age: number = 36
 
@@ -12,8 +42,9 @@ class User {
 	}
 }
 
-const user = new User('João', 36);
+const user: User = new User('João', 36);
 user.showName();
 
 const otherUser = new User('Denize', 37);
 otherUser.showName();
+*/
