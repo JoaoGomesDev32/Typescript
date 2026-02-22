cc1736f93c9e505927ec97dd38fc3297eff207f1
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2026-02-22 15:36:34 +0000

    readonly

--- fundamentos/app.ts
+++ fundamentos/app.ts
@@ -5,5 +5,6 @@ const peopleAccount: PeopleAccount = new PeopleAccount(1, 'João', 10);
 console.log(peopleAccount);
 peopleAccount.deposit();
 
-// const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-// console.log(companyAccount.deposit());
+const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+companyAccount.deposit();
+console.log(companyAccount);
