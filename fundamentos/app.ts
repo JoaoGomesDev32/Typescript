4bdc3fa6b6f9f202227f873e10d4eb47b9b30424
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2026-02-22 15:23:02 +0000

    visibilidade de atributos e metodos

--- fundamentos/app.ts
+++ fundamentos/app.ts
@@ -2,7 +2,8 @@ import { CompanyAccount } from './class/CompanyAccount.ts';
 import { PeopleAccount } from './class/PeopleAccount.ts';
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'João', 10);
+console.log(peopleAccount);
 peopleAccount.deposit();
 
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-console.log(companyAccount.deposit());
+// const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+// console.log(companyAccount.deposit());
