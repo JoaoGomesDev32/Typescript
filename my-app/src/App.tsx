08a1d8a0b6417f3a0a7193688bd0ece823359e74
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2026-02-25 17:09:03 +0000

    propriedades para componentes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,30 @@
 import { Card } from "./components/Card"
+import { Footer } from "./components/Footer"
+import { Header } from "./components/Header"
 
 function App() {
   return (
     <>
-      Hello World!
-      <Card />
-      <Card />
-      <Card />
+      <Header />
+      <div>
+        Hello World!
+        <Card
+          id={1}
+          paragraph='Typescript'
+          details='TS para frontend e backend'
+        />
+        <Card
+          id={2}
+          paragraph='HTML'
+          details='HTML para frontend'
+        />
+        <Card
+          id={3}
+          paragraph='SQL'
+          details='SQL para banco de dados'
+        />
+      </div>
+      <Footer />
     </>
   )
 }
